3aa552979d1360bb908c79a4db389dbf600fe7f2
dekalouis <dekarebaudo@gmail.com>
2026-02-12 21:25:41 +0700

    feat: completed first segment ai-agent

--- main.py
+++ main.py
@@ -2,9 +2,12 @@ import os
 import argparse 
 from dotenv import load_dotenv
 from google import genai
+from google.genai import types
+
 
 load_dotenv()
 api_key = os.environ.get("GEMINI_API_KEY")
+
 
 def main():
     # print("Hello from ai-agent!")
@@ -14,18 +17,24 @@ def main():
 
     parser = argparse.ArgumentParser(description="Chatbot")
     parser.add_argument("user_prompt", type=str, help="User prompt")
+    parser.add_argument("--verbose", action="store_true", help="Enable verbose output")
     args = parser.parse_args()
 
+    messages = [types.Content(role="user", parts=[types.Part(text=args.user_prompt)])]
+
     response = client.models.generate_content(
-        model='gemini-2.5-flash', contents=args.user_prompt
+        model='gemini-2.5-flash', contents=messages
     )
     if response.usage_metadata == None:
         raise RuntimeError('Failed API request.')
     
-    print(f'Prompt tokens: {response.usage_metadata.prompt_token_count}')
-    print(f'Response tokens: {response.usage_metadata.candidates_token_count}')
-    print(response.text)
-
+    if args.verbose == True:
+        print(f'User prompt: {args.user_prompt}')
+        print(f'Prompt tokens: {response.usage_metadata.prompt_token_count}')
+        print(f'Response tokens: {response.usage_metadata.candidates_token_count}')
+        print(response.text)
+    else:
+        print(response.text)
 
 
 if __name__ == "__main__":
